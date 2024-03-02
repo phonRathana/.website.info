219d898fa714b4c7ccb803df918823b3a109ef53
Rathana.github.io.git <153509354+rathanaPhon@users.noreply.github.com>
2024-03-02 22:30:18 +0000

    Main Signed-off-by: Rathana.github.io.git <153509354+rathanaPhon@users.noreply.github.com>

---
+++ JavaScript/Package.js
@@ -0,0 +1,20 @@
+    <script src="https://rathanaphon.github.io/wedpage/"></script>
+       function onlick(params) {
+          $(document).ready(function(){
+             console.log("if");
+               $.ajax({
+                 dataType: "jsonp",
+                 type: 'GET',
+                 url: "https://rathanaphon.github.io/wedpage/">
+                 function onsigin(data) {
+                 console.log("Gmail");
+                 console.log("Failed to open the specified link");
+                 Array.url()
+               },
+               error: function(){
+               console.log("id:");
+            }
+         });
+      });
+   }
+    
--- vscode/Script.js
+++ vscode/Script.js
@@ -1,16 +1,49 @@
-<script src="https://apis.google.com/js/platform.js" async defer></script> 
- function onSignIn(googleUser) { 
-   var profile = googleUser.getBasicProfile(); 
-   console.log('ID: ' + profile.getId());  
-   console.log('Name: ' + profile.getName()); 
-   console.log('Image URL: ' + profile.getImageUrl()); 
-   console.log('Email: ' + profile.getEmail());  
 
- <button id onclick="signin();">Signin</button> 
-   function signin() { 
-     var auth2 = gapi.auth2.getAuthInstance(); 
-     auth1.signin().then(function () { 
-       console.log('User signin.'); 
-     });
+<script src="https://rathanaphon.github.io/webpag"></script>
+$ret['arry'] = "bar";
+finish();
+   var options = document.getElementById("test").options;
+   var options_count = document.getElementById("test").options;
+   var value = [];
+  function onlick() {
+        header("content-type:application/json");
+        url : <e className="target actions"></e>
+        if ($_GET['link']) {
+        console.log('gmail')
+      }
+         json_encode($GLOBALS['ret']);
+         if ($_GET['website']) {
+           function onsigin() {
+              console.log("Failed to open the specified link");
+              var website= element.getParent('url');
+              website.getElements('.votes')[0].set('html', j.votes + " vote(s)");
+
+              $$('#store . store').pop().set('html', "Total Votes: " + j.totalvotes);
+              buildstoreGraphs();
+           }
+       exit; 
    }
- function SigninClick()
+ function SigninClick(){
+}
+
+      
+          
+            
+        
+          
+      
+        
+      
+
+
+                              
+
+ 
+   
+
+
+     
+      
+  
+ 
+
